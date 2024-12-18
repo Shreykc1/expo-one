3c25234c8c50ea541561f23dfa596225ebe78aaa
shreykc1 <shreykc1@gmail.com>
2024-12-18 08:24:50 +0530

    done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,30 @@ import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 const PlaceholderImage = require('@/assets/images/background-image.png')
 import * as ImagePicker from 'expo-image-picker';
+import { useRef, useState } from 'react';
+import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
+import { type ImageSource } from 'expo-image';
+import EmojiList from '@/components/EmojiList'
+import EmojiSticker from "@/components/EmojiSticker";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
 
 export default function Index() {
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+    const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+    const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+    const [status, requestPermission] = MediaLibrary.usePermissions();
+    const imageRef = useRef<View>(null);
+
+    if (status === null) {
+      requestPermission();
+    }
+
+
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
           mediaTypes: ['images'],
@@ -15,26 +37,68 @@ export default function Index() {
 
         if (!result.canceled) {
           console.log(result);
+          setSelectedImage(result.assets[0].uri);
+          setShowAppOptions(true)
         } else {
           alert('You did not select any image.');
         }
       };
 
+      const onReset = () => {
+        setShowAppOptions(false);
+      };
 
+      const onAddSticker = () => {
+        setIsModalVisible(true);
+      };
+
+      const onModalClose = () => {
+        setIsModalVisible(false);
+      };
+
+      const onSaveImageAsync = async () => {
+        try {
+          const localUri = await captureRef(imageRef, {
+            height: 440,
+            quality: 1,
+          });
+
+          await MediaLibrary.saveToLibraryAsync(localUri);
+          if (localUri) {
+            alert('Sticker saved to gallery!');
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
 
 
   return (
-    <View
-      style={styles.container}
-    >
-     <View>
-        <ImageViewer imgSource={PlaceholderImage} />
+    <GestureHandlerRootView style={styles.container}>
+     <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        { pickedEmoji && <EmojiSticker imageSize={50} stickerSource={pickedEmoji}/> }
+        </View>
      </View>
-     <View style={styles.footerContainer}>
-     <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
-        <Button label="Use this photo" />
-      </View>
-    </View>
+     {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      )  : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+      <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+    </GestureHandlerRootView>
   );
 }
 
@@ -68,6 +132,14 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-    marginTop: 30
+    marginBottom: 100
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 })
